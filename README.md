9af35cf23ab47a0d68a30feb01d0578d484291f5
Vladimir Tribusean <vladimir.tribusean@gmail.com>
2018-08-30 16:22:45 +0300

    Refactored and reimplemented the getAvailability function.

--- README.md
+++ README.md
@@ -9,7 +9,11 @@
 >  2. The function that is itterationg through events and poopulates the `Availability` Object works in a wrong way. When an event of kind `appointment` is queryed then it filters already inserted `opening hours`, so if an `appointment` event is inserted before `opening` event (like in the test case 2) then it will try to filter an empty array of `opening hours`, and then will process the `opening` event, that will populate the availability with all `opening hours`.
 >     ##### I've decided to redesign the entire implementation. 
 
+##### 3. Refactoring of implementation function.
 
+> First of all I decided that I need to create an Object that will include all the dates with events, including reccuring events for the entire required period. The key will be the entire date string of format `D-MM-YYYY` this will allow us to fetch availability for years ahead. `weekly_reccuring` means that event is repeating each 7 days. Each day is an Object will contain multiple keys that represents the kind of event, in our case there are only 2 kinds `opening` and `appointment`.
+> Now after we already have `daysWithEvents` Object, we can set the `availability` for the requested time frame. I am setting availbaility by subtracting all `appointment` hours from `opening` hours for each day that has events.
+> ###### Now all initial tests are green, that's just awesome!
 
 
  
--- src/getAvailabilities.js
+++ src/getAvailabilities.js
@@ -1,39 +1,88 @@
 import moment from "moment";
 import knex from "knexClient";
 
-export default async function getAvailabilities(date) {
+const getEvents = (queryDate) => knex
+  .select("kind", "starts_at", "ends_at", "weekly_recurring")
+  .from("events")
+  .where(function() {
+    this.where("weekly_recurring", true).orWhere("ends_at", ">", +queryDate);
+  });
+
+const getHours = (event) => {
+  const date = moment(event.starts_at).subtract(30, 'minutes');
+  const hours = [];
+  while (date.add(30, "minutes").isBefore(event.ends_at)) {
+    hours.push(date.format("H:mm"));
+  }
+
+  return hours;
+}
+
+const getDaysWithEvents = async (queryDate, daysFromDate) => {
+  const events = await getEvents(queryDate);
+
+  const daysWithEvents = events.reduce((collector, event) => {
+    const eventDate = moment(event.starts_at);
+    const weeksToSkip = moment(queryDate).diff(eventDate, 'weeks');
+    const hoursInterval = getHours(event);
+    const eventDates = [eventDate.format("D-MM-YYYY")];
+
+    if (event.weekly_recurring) {
+      eventDate.add(weeksToSkip, 'week');
+      let weeksCount = Math.ceil(daysFromDate / 7);
+      while (weeksCount > 0) {
+        eventDates.push(eventDate.add(1, "weeks").format("D-MM-YYYY"));
+        weeksCount -= 1;
+      }
+    }
+
+    const eventsOnDates = eventDates.reduce((col, date) => {
+      const eventsOnDate = collector[date] || {};
+
+      return {
+        ...col,
+        [date]: { 
+          ...eventsOnDate,  
+          [event.kind]: eventsOnDate[event.kind] ? [
+            ...eventsOnDate[event.kind],
+            ...hoursInterval,
+          ] : hoursInterval,
+        },
+      };
+    }, {});
+
+    return {
+      ...collector,
+      ...eventsOnDates,
+    }
+  }, {});
+
+  return daysWithEvents;
+}
+
+export default async function getAvailabilities(queryDate, daysFromDate = 7) {
   const availabilities = new Map();
-  for (let i = 0; i < 7; ++i) {
-    const tmpDate = moment(date).add(i, "days");
-    availabilities.set(tmpDate.format("d"), {
+
+  const tmpDate = moment(queryDate);
+  for (let i = 0; i < daysFromDate; ++i) {
+    availabilities.set(tmpDate.format("D-MM-YYYY"), {
       date: tmpDate.toDate(),
       slots: []
     });
+    tmpDate.add(1, "days");
   }
 
-  const events = await knex
-    .select("kind", "starts_at", "ends_at", "weekly_recurring")
-    .from("events")
-    .where(function() {
-      this.where("weekly_recurring", true).orWhere("ends_at", ">", +date);
-    });
+  const daysWithEvents = await getDaysWithEvents(queryDate, daysFromDate);
 
-  for (const event of events) {
-    for (
-      let date = moment(event.starts_at);
-      date.isBefore(event.ends_at);
-      date.add(30, "minutes")
-    ) {
-      const day = availabilities.get(date.format("d"));
-      if (event.kind === "opening") {
-        day.slots.push(date.format("H:mm"));
-      } else if (event.kind === "appointment") {
-        day.slots = day.slots.filter(
-          slot => slot.indexOf(date.format("H:mm")) === -1
-        );
-      }
-    }
-  }
+  Object.keys(daysWithEvents).forEach(day => {
+    const availabilityDay = availabilities.get(day);
+    const { opening = [], appointment = [] } = daysWithEvents[day];
+    const availableHours = opening.filter(hour => appointment.indexOf(hour) === -1);
+
+    if (!availabilityDay) return null;
+
+    availabilityDay.slots = [...availabilityDay.slots, ...availableHours];
+  });
 
   return Array.from(availabilities.values())
 }
